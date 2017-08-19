868bb14080976059a657625a7c4517993bc842c6
sadminriley <rileymace57@gmail.com>
2017-08-19 13:15:55 -0400

    Fixed type in README.md

--- setup.py
+++ setup.py
@@ -13,16 +13,15 @@ setup(
     url = "https://github.com/sadminriley/domainchecker",
     license = "MIT",
 )
-<<<<<<< HEAD
+
 '''
 Install domainchecker files and default wordslist. Touches a whitelist file by default.
 
 '''
-os.mkdir('/opt/domainchecker') 
-shutil.move('badwords', '/opt/domainchecker/badwords') 
-os.mknod('/opt/domainchecker/whitelist') 
-  
-=======
+os.mkdir('/opt/domainchecker')
+shutil.move('badwords', '/opt/domainchecker/badwords')
+os.mknod('/opt/domainchecker/whitelist')
+
 # Install domainchecker files/wordslist
 if not os.path.exists("/opt/"):
     os.mkdir("/opt")
@@ -31,4 +30,3 @@ if not os.path.exists("/opt/domainchecker"):
     os.mkdir("/opt/domainchecker")
 
 shutil.move('badwords', '/opt/domainchecker/')
->>>>>>> 911466cf264e167ed749fcb62d034c74394f8d75
